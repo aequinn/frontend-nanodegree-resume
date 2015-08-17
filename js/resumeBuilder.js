5c18f364fffdb8ce5064a4fa6d4f94163bb39395
Anthony Quinn <quinn.ant@gmail.com>
2015-08-17 15:24:12 -0400

    Clean up resumeBuilder.js to clean out old test code. Define all objects to be used. Old code saved in backup

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,7 @@
 /*
-Using object literal notation to create an object
+Using object literal notation to create an objects. Object with peronsal information
  */
- var bio ={
+ var bio = {
    "name" : "Anthony Quinn",
    "role" : "Product Manager",
    "contacts" :
@@ -12,61 +12,104 @@ Using object literal notation to create an object
        "location" : "Ann Arbor, MI"
      },
    "pictureURL" : "images/200x200.jpg",
-   "welcome" : "I'm Anthony and I like to build things that help solve problems",
-   "skills" : ["UX Design","Project Management","HTML","JavaScript"]
- }
-/* Using the dot notation to create an object*/
-var work = {};
-work.position = "Project Manager";
-work.employer = "General Electric";
-work.yearWorked = "5";
-work.address = "Van Buren Township, MI"
+   "welcomeMessage" : "I'm Anthony and I like to build things that help solve problems",
+   "skills" :
+   [
+     "UX Design",
+     "Project Management",
+     "HTML",
+     "JavaScript"
+   ]
+ };
+/* A work history object, created using literal notation*/
+var work = {
+  "jobs" : [
+    {
+      "employer" : "General Electric Corp.",
+      "title" : "Projecct Manager",
+      "location" : "Van Buren Township, MI",
+      "datesWorked" : "Feb 2015 - Present",
+      "description" : "Manage the maturation of an internal web application started by the Preformance Development HR team for supporting leadership development programs at GE. Own the releationship with customers and stakeholders, responsible for develivering roadmap, technical strategy, design, and leading the development team. "
+    },
+    {
+      "employer" : "General Electric Capital Corp.",
+      "title" : "Interaction Designer",
+      "location" : "Van Buren Township, MI",
+      "datesWorked" : "Aug 2012 - Feb 2015",
+      "description" : "User Experience advocate and representative on various projects for GE Capital. Responsibilites included user research, wireframming, managing timelines and deliverables, "
+    },
+    {
+      "employer" : "General Electric Capital Corp",
+      "title" : "Information Technology Leadership Program Associate",
+      "location" : "Various",
+      "datesWorked" : "July 2010 - Aug 2012",
+      "description" : "Rotated through various roles and teams taking on impact projects, meeting deliveralbes on demanding timelines."
+    }
+  ]
+};
 
-/* Using the bracket notation to create an object*/
-var education = {};
-education['school'] = "Carnegie Mellon University";
-education["major"] = "Master of Information Systems Management";
-education["yearsAttended"] = 2;
-education["address"] = "Pittsburgh, PA";
-
-
-/*Replace placeholders from template with object values*/
- var formattedName = HTMLheaderName.replace("%data%",bio.name);
- var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
- var formattedPicture = HTMLbioPic.replace("%data%",bio.pictureURL);
- var formattedGeneric = HTMLcontactGeneric.replace("%contact%","Email:");
- formattedGeneric = formattedGeneric.replace("%data%",bio.contact);
- var formattedEmail = HTMLmobile.replace("%data", bio.contacts.mobile);
- var formattedMobile = HTMLemail.replace("%data%", bio.contacts.email);
- var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
- var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
- var formattedWelcome = HTMLwelcomeMsg.replace("%data%", bio.welcome);
-
- /*Work swap*/
- var formattedWorkName = HTMLworkEmployer.replace("%data%", work.employer);
-
-/*Eductaion Info*/
-var formattedSchoolName = HTMLschoolName.replace("%data%", education["school"]);
-var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education["major"]);
-
-/*Append template and other values to DOM*/
- $("#header").prepend(formattedRole);
- $("#header").prepend(formattedName);
- $("#header").prepend(formattedPicture);
- $("#topContacts").append(formattedEmail);
- $("#topContacts").append(formattedMobile);
- $("#topContacts").append(formattedGithub);
- $("#topContacts").append(formattedLocation);
- $("#header").append(formattedWelcome);
- $("#header").append(HTMLskillsStart);
- for(i=0; i<bio.skills.length; i++){
-   var formattedSkills = HTMLskills.replace("%data%",bio.skills[i]);
-   $("#skills").append(formattedSkills);
- }
-
-$("#workExperience").append(HTMLworkStart);
-$("#workExperience").append(formattedWorkName);
-$("#education").append(HTMLschoolStart);
-$("#education").append(formattedSchoolName);
-$("#education").append(formattedSchoolDegree);
-console.log(formmatedSchoolName+' '+formattedSchoolDegree);
+/*Projects object*/
+var projets = {
+  "projects" : [
+    {
+      "title" : "Masters Capstone Project",
+      "datesWorked" : "Sept 2009 - Dec 2009",
+      "description" : "Lead a team of six in research on the internet of things and home automation",
+      "images" : [ ]
+    }]
+  };
+/* Education Object*/
+var education = {
+  "schools" : [
+    {"name" : "Carnegie Mellon Unversity",
+    "degree" : "Master",
+    "dates" : "2008-2009",
+    "location" : "Pittsburgh, PA",
+    "majors" : ["Information Systems Management"],
+    "url" : "http://www.heinz.cmu.edu/school-of-information-systems-and-management/index.aspx"
+    },
+    {"name" : "Brigham Young University",
+      "degree" : "Bachealors",
+      "dates" : "2002-2008",
+      "location" : "Provo, UT",
+      "majors" :
+      [
+        "Information Systmes",
+        "International Development"
+      ],
+      "url" : "http://marriottschool.byu.edu/bsis/"
+    }
+  ],
+  "onlineCourses" : [
+    {
+      "title" : "Front-End Developer Nano-degree",
+      "school"  : "Udacity",
+      "datesAttended" : "May 2015 - Present",
+      "url" : "https://www.udacity.com/nanodegree"
+    },
+    {
+      "title" : "Shaping up with Angular.js",
+      "school"  : "Code School",
+      "datesAttended" : "Nov 2014",
+      "url" : "http://campus.codeschool.com/courses/shaping-up-with-angular-js/intro"
+    },
+    {
+      "title" : "Javascrip, HTML, CSS, & jQuery",
+      "school"  : "Codecademy",
+      "datesAttended" : "Nov 2014 - Present",
+      "url" : "https://www.codecademy.com/aequinn"
+    },
+    {
+      "title" : "Computer Science 101",
+      "school"  : "Coursera - Stanford University",
+      "datesAttended" : "May 2013",
+      "url" : "https://www.coursera.com"
+    },
+    {
+      "title" : "Creative Programming for Digital Media & Mobile Apps",
+      "school"  : "Coursera - University of London",
+      "datesAttended" : "September 2013",
+      "url" : "https://www.coursera.com"
+    }
+  ]
+};
